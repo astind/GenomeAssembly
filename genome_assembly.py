41650c42a0037b87daa51487d48b3cf0ace0002f
Dillon Astin <astindillon@gmail.com>
2018-11-09 11:06:17 -0700

    set up way to print all results to a text file

--- genome_assembly.py
+++ genome_assembly.py
@@ -48,9 +48,8 @@ def check_errors(kmers, limit):
     return new_kmers
 
 
-if __name__ == '__main__':
-    contigs = do_sample(sys.argv[1], sys.argv[2])
-    sizes = []
+def run_tests(filename, error_limit):
+    contigs = do_sample(filename, error_limit)
     longest_contig = 0
     best_set = None
     kmer_size = None
@@ -61,15 +60,63 @@ if __name__ == '__main__':
                 longest_contig = length
                 best_set = contig
                 kmer_size = kmer
+    return longest_contig, best_set, kmer_size
 
-    print('Longest contig size: ' + str(longest_contig))
-    print('Number of contigs: ' + str(len(best_set)))
+
+def format_output(file, longest_contig, best_set, kmer_size, error_limit):
+    file.write('Kmer size: ' + str(kmer_size) + '\n')
+    if error_limit != 'na':
+        file.write('Error limit: ' + str(error_limit) + '\n')
+    file.write('Longest contig size: ' + str(longest_contig) + '\n')
+    file.write('Number of contigs: ' + str(len(best_set)) + '\n')
     average = 0
+    file.write('Best Set:' + '\n')
     for x in best_set:
+        file.write(x + '\n')
         average += len(x)
     average = average / len(best_set)
-    print('Average Contig size: ' + str(average))
-    print('Kmer size: ' + str(kmer_size))
-    print('Best Set:')
-    print(best_set)
+    file.write('Average Contig size: ' + str(average) + '\n')
+
+
+def print_results():
+    with open('student_assembler_results.txt', "w") as file:
+        file.write('Gennome Assembly Project Results: \n \n')
+        file_list = ['synthetic.example.noerror.small.fasta', 'synthetic.noerror.small.fasta', 'synthetic.noerror.large.fasta',
+                     'real.error.small.fasta', 'real.error.large.fasta']
+        error_list = ['na', 'na', 'na', 2, 3]
+        i = 0
+        for f in file_list:
+            file.write(f + '\n')
+            l_c, b_s, k_s = run_tests(f, error_list[i])
+            format_output(file, l_c, b_s, k_s, error_list[i])
+            i += 1
+            file.write('\n')
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'run_all':
+        print_results()
+    else:
+        contigs = do_sample(sys.argv[1], sys.argv[2])
+        longest_contig = 0
+        best_set = None
+        kmer_size = None
+        for kmer, contig in contigs.items():
+            for seq in contig:
+                length = len(seq)
+                if length > longest_contig:
+                    longest_contig = length
+                    best_set = contig
+                    kmer_size = kmer
+
+        print('Longest contig size: ' + str(longest_contig))
+        print('Number of contigs: ' + str(len(best_set)))
+        average = 0
+        for x in best_set:
+            average += len(x)
+        average = average / len(best_set)
+        print('Average Contig size: ' + str(average))
+        print('Kmer size: ' + str(kmer_size))
+        print('Best Set:')
+        print(best_set)
 
